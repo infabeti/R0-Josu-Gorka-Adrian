2a71976b888dbf1956e4f4b619b05056f7e572b3
Adrian <adrian.martineztr@elorrieta-errekamari.com>
2020-09-30 09:06:45 +0200

    Librerias y app

--- Biblioteca/src/main/java/reto/Lector.java
+++ Biblioteca/src/main/java/reto/Lector.java
@@ -22,85 +22,81 @@ public class Lector {
 	public String leerTeclado() {
 		String entrada = "";
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la Ruta del fichero que quieres Leer: ");	
 		entrada = sc.next();
-		
 		return entrada;
 	}
 	
-	public String leerDOC_DOCX(){
-		Scanner sc = new Scanner(System.in);
-		String ruta;
-		String entrada = "";
-		boolean encontrado = false;
-		while(!encontrado) {
+	public boolean Encontrar(String ruta) {
+			
+		File archivo = new File(ruta);
+		if (!archivo.exists()) {
+			return true;
+		}
+		else {
+		return true;
+		}
+	}
+	
+	public String leerDOC(String ruta){
+		String entrada = "";	
 			try {
-				System.out.println("Introduce la ruta del archivo que quieras leer");
-				ruta = sc.next();
 				File fichero = new File(ruta);
 				FileInputStream fis = new FileInputStream(fichero);
-				encontrado = true;
-				
-				if(ruta.endsWith(".doc")) {
 					HWPFDocument hwpfd = new HWPFDocument(fis);
 					WordExtractor we = new WordExtractor(hwpfd);
-					entrada = we.getText();
-					
-				}else if(ruta.endsWith(".docx")) {
-					XWPFDocument documentX = new XWPFDocument(fis);         
-					XWPFWordExtractor ex = new XWPFWordExtractor(documentX);
-					entrada = ex.getText();
-				}
-				
+					entrada = we.getText();		
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				System.out.println("La ruta no corresponde a ningun archivo");
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+
+		System.out.println(entrada);	
+		return entrada;
+	}
+	
+	public String leerDOCX(String ruta){
+		String entrada = "";
+			try {
+				File fichero = new File(ruta);
+				FileInputStream fis = new FileInputStream(fichero);
+					XWPFDocument documentX = new XWPFDocument(fis);         
+					XWPFWordExtractor ex = new XWPFWordExtractor(documentX);
+					entrada = ex.getText();
 		
+			} catch (FileNotFoundException e) {
+				System.out.println("La ruta no corresponde a ningun archivo");
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}		
 		System.out.println(entrada);
 		
 		return entrada;
 	}
 	
-	public String leerPDF() {
-		Scanner sc = new Scanner(System.in);
+	public String leerPDF(String ruta) {
 		try {
-			
-			System.out.println("Escribe la ruta del archivo que quieres leer");
-			String URL = sc.next();
-			PDFText pdfText = new PDFText (URL, null);
-			
-			String docText = pdfText.getText();
-			
-			
-			
-			System.out.printf(docText);
-			
+			PDFText pdfText = new PDFText (ruta, null);			
+			String docText = pdfText.getText();				
+			System.out.printf(docText);			
 			return(docText);
 			
 		} catch (Throwable t) {
-			// TODO Auto-generated catch block
+			
 			t.printStackTrace();
 		}
 		return null;
 		
 	}
 	
-	public String leerXML() {
-		Scanner sc = new Scanner(System.in);
+	public String leerXML(String ruta) {
 		String entrada = "";
-		String ruta;
 		Document doc;
-		Node node;
-		String datos_nodo[] = null;
 		Node ntemp;
-		
-		System.out.println("Introduce la ruta del archivo que quieras leer");
-		ruta = sc.next();
+
 		File fichero = new File(ruta);
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
